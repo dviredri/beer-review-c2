e3a8918fbc8a54104c42b980fe74ea3194239ae3
Dvir Edri <dviredri433@gmail.com>
2017-06-04 14:23:11 +0300

    saving

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function addBeer(name, category) {
+function addBeer(name, category, rating) {
   beers.push({
     name,
-    category
+    category,
+    rating
   });
 
 };
 
 $('.btn').click(function() {
 
-  addBeer($('.category-input').val(), $('.beer-input').val());
+  addBeer($('.category-input').val(), $('.beer-input').val(), $('.rating-input').val());
+  $('.category-input').val("");
+  $('.beer-input').val("");
+  $('.rating-input').val("");
   updateBeers();
 });
 
@@ -17,15 +21,6 @@ var beers = [];
 function updateBeers() {
   $(".beers-list").empty()
   for (i = 0; i < beers.length; i++)
-    $(".beers-list").append("<li>" + beers[i].name+" "+beers[i].category + '<input id="input'+i+'" type="number" />'+
-
-    "</li>"+"<li>"+"This Beer rating is:"+ratings+"</li>"+'<li>'+'<button class="btnAddRating">submit Rating</button>'+'</li>');
+    $(".beers-list").append("<li>" + beers[i].name+" "+beers[i].category+"     This Beer rating is:"+ beers[i].rating + '</li>');
 
 }
-
-var ratings="not rated"
-$('.body').on('click', '.btnAddRating', function() {
-
-  var a=
-  console.log(a);
-});
